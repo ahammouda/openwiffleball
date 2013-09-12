d82b1cd635f508ca7f8ac83f7b4feaf0881967c7
Adam Hammouda <adamhammouda3@gmail.com>
2013-09-11 23:48:34 -0500

    changed a setting

--- wiffleball/settings.py
+++ wiffleball/settings.py
@@ -163,12 +163,12 @@ if os.environ['LOCAL_MACHINE']=='true':
     #EMAIL_HOST_USER='adamhammouda3'
     #EMAIL_HOST_PASSWORD='password'
 else:
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = 'adamhammouda3@gmail.com'
     EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
     DEFAULT_FROM_EMAIL = 'adamhammouda3@gmail.com'
-    EMAIL_USE_TLS = True
 
 
 if os.environ['LOCAL_MACHINE']=='true':
