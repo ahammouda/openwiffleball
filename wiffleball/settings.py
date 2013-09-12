65d9bc4032fe16ae9939a67886a27c99abecd59c
Adam Hammouda <adamhammouda3@gmail.com>
2013-09-12 00:11:52 -0500

    Testing django-email take 100

--- wiffleball/settings.py
+++ wiffleball/settings.py
@@ -165,9 +165,9 @@ if os.environ['LOCAL_MACHINE']=='true':
 else:
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
     EMAIL_HOST_USER = 'adamhammouda3@gmail.com'
     EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-    EMAIL_PORT = 587
     DEFAULT_FROM_EMAIL = 'adamhammouda3@gmail.com'
 
 
